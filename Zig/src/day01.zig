622e5896f32f39b970e45afee05c2b1d78e0ec36
Chris3606 <cridley106@gmail.com>
2025-12-01 17:58:35 -0600

    Lots of code cleanup, and a pure mathematical solution.

--- Zig/src/day01.zig
+++ Zig/src/day01.zig
@@ -3,45 +3,73 @@ const std = @import("std");
 const util = @import("util.zig");
 
 const data = @embedFile("data/day01.txt");
+const InputType = std.ArrayList(struct { i8, i32 });
+const max_number = 99;
 
-fn wrapAround(num: i32, wrapTo: i32) i32 {
-    return @mod(@mod(num, wrapTo) + wrapTo, wrapTo);
+fn parseInput(gpa: std.mem.Allocator, input_data: []const u8) !InputType {
+    var vec: InputType = .empty;
+    errdefer vec.deinit(gpa);
+
+    var input_it = util.getLines(input_data);
+    while (input_it.next()) |line| {
+        try vec.append(
+            gpa,
+            .{
+                if (line[0] == 'L') -1 else 1,
+                try std.fmt.parseInt(i32, line[1..], 10),
+            },
+        );
+    }
+
+    return vec;
+}
+
+fn part1(input: InputType) !u32 {
+    var current_value: i32 = 50;
+    var times_landed_on_zero: u32 = 0;
+    for (input.items) |instruction| {
+        const shifts = instruction[1] * instruction[0];
+        current_value = util.wrapAround(current_value + shifts, max_number + 1);
+        if (current_value == 0) {
+            times_landed_on_zero += 1;
+        }
+    }
+    return times_landed_on_zero;
+}
+
+fn part2(input: InputType) !u32 {
+    var current_value: i32 = 50;
+    var times_passed_zero: u32 = 0;
+    for (input.items) |instruction| {
+        const shifts = instruction[1] * instruction[0];
+
+        // Count full wraps
+        times_passed_zero += @divTrunc(@abs(shifts), max_number + 1);
+
+        // Update value
+        const old_current = current_value;
+        current_value = util.wrapAround(current_value + shifts, max_number + 1);
+
+        // Find if we passed zero in a partial wrap; if we're on 0, that's captured later
+        if (old_current != 0 and current_value != 0) {
+            if ((shifts < 0 and current_value > old_current) or (shifts > 0) and current_value < old_current) {
+                times_passed_zero += 1; // Wrapped on a final remainder of shifts <= max_number
+            }
+        }
+
+        // Also if we're on zero, obviously we're at 0
+        if (current_value == 0) {
+            times_passed_zero += 1;
+        }
+    }
+    return times_passed_zero;
 }
 
 pub fn main() !void {
-    var seq = std.mem.tokenizeScalar(u8, data, '\n');
+    const input = try parseInput(util.gpa, data);
 
-    var val: i32 = 50;
-    var times_at_zero: u32 = 0;
-    var times_passing_zero: u32 = 0;
-    while (seq.next()) |line| {
-        var turns = try std.fmt.parseInt(i32, line[1..], 10);
-        while (turns > 0) : (turns -= 1) {
-            if (line[0] == 'L') {
-                val -= 1;
-                if (val < 0) {
-                    val = 99;
-                }
-            } else {
-                val += 1;
-                if (val > 99) {
-                    val = 0;
-                }
-            }
-
-            if (val == 0) {
-                times_passing_zero += 1;
-            }
-        }
-
-        // times_passing_zero += @abs(@divTrunc((turns + val), 100));
-        // val = wrapAround(val + turns, 100);
-        if (val == 0) {
-            times_at_zero += 1;
-        }
-    }
-    std.debug.print("Part 1: {d}\n", .{times_at_zero});
-    std.debug.print("Part 2: {d}\n", .{times_passing_zero});
+    std.debug.print("Part 1: {d}\n", .{try part1(input)});
+    std.debug.print("Part 1: {d}\n", .{try part2(input)});
 }
 
 // Generated from template/template.zig.
--- Zig/src/util.zig
+++ Zig/src/util.zig
@@ -1,39 +1,14 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = gpa_impl.allocator();
 
-// Add utility functions here
+/// Wraps a number around as if by modulo, but works for negative numbers.
+pub fn wrapAround(num: i32, wrapTo: i32) i32 {
+    return @mod(@mod(num, wrapTo) + wrapTo, wrapTo);
+}
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+/// Gets an iterator which breaks the input up into lines and returns each line.
+pub fn getLines(input: []const u8) std.mem.TokenIterator(u8, .scalar) {
+    return std.mem.tokenizeScalar(u8, input, '\n');
+}
