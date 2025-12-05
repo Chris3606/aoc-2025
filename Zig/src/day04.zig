ee453831da8edd9380dd667575889aa6665c8141
Chris3606 <cridley106@gmail.com>
2025-12-04 20:09:18 -0600

    Day 4 part 2.

--- Zig/src/day04.zig
+++ Zig/src/day04.zig
@@ -9,47 +9,49 @@ const InputType = util.SliceView(u8);
 
 const parseInput = util.parseSliceViewFromCharGrid;
 
-fn part1(gpa: std.mem.Allocator, input: InputType) !u32 {
-    _ = gpa;
-    var result: u32 = 0;
+fn removePaper(comptime TContext: type, gpa: std.mem.Allocator, map: *util.SettableGridView(TContext, u8)) !u32 {
+    var rolls_removed: u32 = 0;
+    var pos_it = map.positions();
 
-    // std.debug.print("\n", .{});
-    // var y: u32 = 0;
-    // while (y < input.height()) : (y += 1) {
-    //     var x: u32 = 0;
-    //     while (x < input.width()) : (x += 1) {
-    //         std.debug.print("{c}", .{input.getValueXY(@intCast(x), @intCast(y))});
-    //     }
-    //     std.debug.print("\n", .{});
-    // }
-
-    var pos_it = input.positions();
+    var positions_removed: std.ArrayList(util.Point) = .empty;
+    defer positions_removed.deinit(gpa);
 
     while (pos_it.next()) |pos| {
-        if (input.getValuePoint(pos) != '@') continue;
+        if (map.getValuePoint(pos) != '@') continue;
 
         var surrounding_rolls: u32 = 0;
         for (util.Direction.eightWay) |dir| {
             const neighbor = pos.addDirection(dir);
-            if (!input.contains(neighbor)) continue;
+            if (!map.contains(neighbor)) continue;
 
-            if (input.getValuePoint(neighbor) == '@') surrounding_rolls += 1;
+            if (map.getValuePoint(neighbor) == '@') surrounding_rolls += 1;
         }
 
         if (surrounding_rolls < 4) {
-            result += 1;
+            rolls_removed += 1;
+            try positions_removed.append(gpa, pos);
         }
     }
 
-    return result;
+    for (positions_removed.items) |pos| {
+        map.setValuePoint(pos, '.');
+    }
+
+    return rolls_removed;
 }
 
-fn part2(gpa: std.mem.Allocator, input: InputType) !u64 {
-    _ = gpa;
-    _ = input;
-    var result: u64 = 0;
+fn part1(gpa: std.mem.Allocator, input: *InputType) !u32 {
+    return try removePaper(util.SliceViewContext(u8), gpa, input);
+}
 
-    result += 0; // Get compiler to stop complaining about const
+fn part2(gpa: std.mem.Allocator, input: *InputType) !u32 {
+    var result: u32 = 0;
+
+    var removed: u32 = 1; // Non-zero to force first iteration
+    while (removed != 0) {
+        removed = try removePaper(util.SliceViewContext(u8), gpa, input);
+        result += removed;
+    }
 
     return result;
 }
@@ -59,23 +61,26 @@ pub fn main() !void {
         const status = util.gpa_impl.deinit();
         std.debug.assert(status != .leak);
     }
-    const input = try parseInput(util.gpa, data);
+    var input = try parseInput(util.gpa, data);
     defer input.deinit();
 
-    std.debug.print("Part 1: {d}\n", .{try part1(util.gpa, input)});
-    std.debug.print("Part 2: {d}\n", .{try part2(util.gpa, input)});
+    std.debug.print("Part 1: {d}\n", .{try part1(util.gpa, &input)});
+
+    input.deinit();
+    input = try parseInput(util.gpa, data);
+    std.debug.print("Part 2: {d}\n", .{try part2(util.gpa, &input)});
 }
 
 test "day04_part1" {
-    const input = try parseInput(std.testing.allocator, sample_data);
+    var input = try parseInput(std.testing.allocator, sample_data);
     defer input.deinit();
-    try std.testing.expectEqual(13, try part1(std.testing.allocator, input));
+    try std.testing.expectEqual(13, try part1(std.testing.allocator, &input));
 }
 
 test "day04_part2" {
-    const input = try parseInput(std.testing.allocator, sample_data);
+    var input = try parseInput(std.testing.allocator, sample_data);
     defer input.deinit();
-    try std.testing.expectEqual(0, try part2(std.testing.allocator, input));
+    try std.testing.expectEqual(43, try part2(std.testing.allocator, &input));
 }
 
 // Generated from template/template.zig.
